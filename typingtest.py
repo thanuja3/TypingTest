f4d269270aed6d84baaf0572d256c7e12c2ebce2
Thanuja <69900324+thanuja3@users.noreply.github.com>
2023-09-10 09:22:09 +0530

    typingtest.py

---
+++ typingtest.py
@@ -0,0 +1,30 @@
+"""Typing Test""" 
+
+from time import time
+
+print('press enter to start typing')
+print('Also can use enter to go to the next line')
+print('press enter twice to exit the typing test')
+
+input('-------------')
+
+
+start = time()
+
+text =[]
+while True:
+    line = input()
+    if not line:
+        break
+    text.append(line)
+
+end = time()
+
+print('------------')
+
+time_taken = (end-start) /60
+char_count = sum(len(item) for item in text)
+words_count = char_count /4
+
+wpm = words_count/time_taken
+print(f'Average wordperminute is {wpm}')
